76998ac9edc0a2251de227d66a7c056d77ab3c97
Nikolay <rzaevnr@list.ru>
2019-06-22 12:14:23 +0300

    Correction

--- routes/priem.js
+++ routes/priem.js
@@ -30,6 +30,7 @@ router.route('/specialities').get((req, res, next) => {
 });
 
 //Переделать!!!!!
+//ДЛЯ коммита
 router.route('/newSpecialities').get((req, res, next) => {
   pool.connect(err => {
     if (err) res.sendStatus(400);
